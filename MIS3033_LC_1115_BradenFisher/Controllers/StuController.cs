0e5220d10de2f72219f0bd8c86fa5b007d16e257
BradenFish <braden.j.fisher-1@ou.edu>
2023-11-20 09:33:48 -0600

    10/20 lecture code prepare

---
+++ MIS3033_LC_1115_BradenFisher/Controllers/StuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIS3033_LC_1115_BradenFisher.Controllers
+{
+    public class StuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
