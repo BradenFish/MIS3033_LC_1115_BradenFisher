33021ed5300a1d209821a61f7cddc5d0fab48fcf
BradenFish <braden.j.fisher-1@ou.edu>
2023-11-27 08:45:15 -0600

    11/27 lecture code prepare

--- MIS3033_LC_1115_BradenFisher/Controllers/StuController.cs
+++ MIS3033_LC_1115_BradenFisher/Controllers/StuController.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MIS3033_LC_1115_BradenFisher.Data;
 
 namespace MIS3033_LC_1115_BradenFisher.Controllers
 {
-    public class StuController : Controller
+    public class StuController : Controller// Controller name is Stu
     {
-        public IActionResult Index()
+        // IActionResult: View, web page
+        // JsonResult: web api
+
+        StuDB db = new StuDB();// StuDB, Complex, memberfield
+
+        public JsonResult GetStus()// function name: GetStus, web api
+        {
+            var r = db.Students.Select(x => new {Id=x.Id, Name=x.Name, favPlace=x.favPlace, DOB=x.DOB.ToString("MM/dd/yyyy"), lat=x.lat, lon=x.lon  });
+            return Json(db.Students);
+        }
+        
+        public IActionResult Index()// function, action,
         {
             return View();
         }
